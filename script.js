d5436ed017f9dc44c7b278aaed77ce81f6b97a48
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-30 23:22:12 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var input = document.getElementById("no");
+input.addEventListener("keypress", function(event){
+    if(event.key==='Enter'){
+        event.preventDefault();
+        document.getElementById("submit").click();
+    }
+});
+function calc(){
+var n = document.getElementById("no").value;
+for(i=1; i<=10; i++){
+    document.getElementById("display").innerHTML += `${n} * ${i} = ${n*i} <br>`;
+    
+};
+
+} 
