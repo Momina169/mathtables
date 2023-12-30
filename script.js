84d62573bd274ad75424ba835cf1dc393f4ee903
Momina Ijaz <mominaijaz169@gmail.com>
2023-12-30 23:51:18 +0500

    updated

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ input.addEventListener("keypress", function(event){
         document.getElementById("submit").click();
     }
 });
+
+function clearScreen(){
+    document.getElementById("display").innerHTML = "";
+}
 function calc(){
 var n = document.getElementById("no").value;
 for(i=1; i<=10; i++){
     document.getElementById("display").innerHTML += `${n} * ${i} = ${n*i} <br>`;
     
 };
-
 } 
